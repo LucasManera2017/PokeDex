f7f2b48cf24e0ab09558493c1f5e2edc38f12f9e
Lucas Manera <lucas.manera2017@gmail.com>
2025-06-12 09:17:53 -0300

    feat: started request JSON

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-console.log('Sucesso');
+
+const offset = 0;
+const limit = 10; 
+
+const url =  `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+//Retorna uma promise (assincronismo)
+fetch(url)
+  .then((response) => {//arrow function
+    return response.json()//conversão do body para json
+  })
+  .then(function(jsonBody){
+    console.log(jsonBody)
+  })
+  .catch(function(error) {
+    console.log(error)
+  })
+  .finally(function() {
+    console.log('Requisição concluída');
+  })
+
