4b1c4beb82bb5b1eff8163cf2b1b6f8a00d17d9f
Lucas Manera <lucas.manera2017@gmail.com>
2025-06-20 14:31:28 -0300

    feat: finished working with promisse All

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,17 @@
 const pokeApi = {}
 
+
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url)
+          .then(response => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
   return fetch(url)
     .then((response) => response.json()) //conversão do body para json e retorna com função arrow
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error))
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
 }
